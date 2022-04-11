3739eaad99deaf4b7a6cc30a4502cd9859cf7b6d
Leonardo Coutinho dos Santos <leonardo.coutinho@vetta.digital>
2022-04-11 07:08:32 -0300

    first draft - introduction - beginning carrossel

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 let containerDivElement = document.body.querySelector('.container');
-let h1Element = document.body.querySelector('.text-color');
+let h1Element = document.body.querySelector('.text');
 
 let hoverFunction = () => {
   if (containerDivElement.classList[1] === 'bg') {
     containerDivElement.classList.remove('bg');
     containerDivElement.classList.add('bg-1');
-    h1Element.color = '#ebebeb';
+    h1Element.innerHTML = 'Seja bem-vindo ;)';
   } else if (containerDivElement.classList[1] === 'bg-1') {
     containerDivElement.classList.remove('bg-1');
     containerDivElement.classList.add('bg-2');
-    h1Element.color = 'royalblue';
+    h1Element.innerHTML = 'Pronto para começar?';
   } else if (containerDivElement.classList[1] === 'bg-2') {
     containerDivElement.classList.remove('bg-2');
     containerDivElement.classList.add('bg');
-    h1Element.color = 'tomato';
+    h1Element.innerHTML = 'Clique aqui!';
   }
 };
 
